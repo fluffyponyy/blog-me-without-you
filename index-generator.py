3e5c58bc43923e37a58e2f47609ef03dbcf9f663
Ana <emilymooshroom@gmail.com>
2025-04-23 22:07:14 -0700

    added call to date/archive generator so it updates with each index.html update

--- index-generator.py
+++ index-generator.py
@@ -1,4 +1,5 @@
 import os
+import date_extractor
 
 s = open("html/index_start.html", "r")
 html_start = s.read()
@@ -18,9 +19,13 @@ directory = os.fsencode(directory_name)
     
 for file in os.listdir(directory):
     filename = os.fsdecode(file)
-    blog_post = open(directory_name + filename, "r")
-    generated_file = "<div class=post>" + blog_post.read() + "</div>" + generated_file
-    blog_post.close()
+    blog_file = open(directory_name + filename, "r")
+    blog_post = blog_file.read()
+
+    date_extractor.get_date(blog_post, filename)
+
+    generated_file = "<div class=post>" + blog_post + "</div>" + generated_file
+    blog_file.close()
 
 generated_file = html_start + generated_file + html_end
 i.write(generated_file)
